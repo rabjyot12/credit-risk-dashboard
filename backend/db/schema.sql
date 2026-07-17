e35cd7af0de7f24f596ed49ad3e3cd11bcd90103
rabjyot12 <rabjyotsinghm12@gmail.com>
2026-07-17 20:04:31 +0530

    added '_' to each name and also changed number_of_dependents dtype to integer

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -3,16 +3,16 @@ CREATE TABLE applications (
     version INTEGER NOT NULL DEFAULT 1,
     previous_application_id UUID REFERENCES applications(application_id),
 
-    RevolvingUtilizationOfUnsecuredLines REAL,
-    age INTEGER,
-    "NumberOfTime30-59DaysPastDueNotWorse" INTEGER,
-    DebtRatio REAL,
-    MonthlyIncome REAL,
-    NumberOfOpenCreditLinesAndLoans INTEGER,
-    NumberOfTimes90DaysLate INTEGER,
-    NumberRealEstateLoansOrLines INTEGER,
-    "NumberOfTime60-89DaysPastDueNotWorse" INTEGER,
-    NumberOfDependents REAL,
+	revolving_utilization_unsecured_lines REAL,
+	age INTEGER,
+	number_of_time_30_59_days_past_due_not_worse INTEGER,
+	debt_ratio REAL,
+	monthly_income REAL,
+	number_of_open_credit_lines_and_loans INTEGER,
+	number_of_times_90_days_late INTEGER,
+	number_real_estate_loans_or_lines INTEGER,
+	number_of_time_60_89_days_past_due_not_worse INTEGER,
+	number_of_dependents INTEGER,
 
     submitted_at TIMESTAMPTZ NOT NULL DEFAULT now()
 )
