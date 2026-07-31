027f43db38725da6225822d26a039817dcab41ae
rabjyot12 <rabjyotsinghm12@gmail.com>
2026-07-31 14:09:32 +0530

    this is databse manager. it's only job is to link python to postgresql

---
+++ backend/db/database.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+def get_connection():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
+    return conn
+
